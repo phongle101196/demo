319f0a0edf2f4718e3f03a05233f299a81a3aca0
phongle101196 <saymesach@gmail.com>
2025-03-17 09:04:24 +0700

    update file sql

--- test_database.sql
+++ test_database.sql
@@ -1,9 +1,11 @@
 drop database if exists test;
-create database if not exists test;
+create database if not exists test
+character set utf8mb4
+collate utf8mb4_unicode_ci;
 use test;
 
 create table users(
-	id 			int primary key auto_increment,
+	id 			varchar(36) primary key,
     username 	varchar(50) unique not null,
     email		varchar(100) unique not null,
     `password`	varchar(255) not null,
@@ -15,29 +17,29 @@ create table users(
     `role`		enum('USER', 'POSTER', 'ADMIN') default 'USER'
 );
 
-create table categories(
-	id			int primary key auto_increment,
-    `name`		varchar(100) unique not null
-);
+-- create table categories(
+-- 	id			int primary key auto_increment,
+--     `name`		varchar(100) unique not null
+-- );
 
 
-create table posts(
-	id			int primary key auto_increment,
-    title		varchar(255) not null,
-    content		text not null,
-    price		decimal(12,2) not null,
-    area		decimal(6,2) not null,
-    address		varchar(100) not null,
-    create_date	datetime default current_timestamp,
-    user_id		int not null,
-    category_id	int not null,
-    foreign key (user_id) 		references users(id),
-    foreign key (category_id)	references categories(id)
-);
+-- create table posts(
+-- 	id			int primary key auto_increment,
+--     title		varchar(255) not null,
+--     content		text not null,
+--     price		decimal(12,2) not null,
+--     area		decimal(6,2) not null,
+--     address		varchar(100) not null,
+--     create_date	datetime default current_timestamp,
+--     user_id		char(36) not null,
+--     category_id	int not null,
+--     foreign key (user_id) 		references users(id),
+--     foreign key (category_id)	references categories(id)
+-- ); 
 
-create table images(
-	id			int primary key auto_increment,
-    img_url		varchar(255) not null,
-    post_id		int not null,
-    foreign key (post_id) references posts(id)
-);
+-- create table images(
+-- 	id			int primary key auto_increment,
+--     img_url		varchar(255) not null,
+--     post_id		int not null,
+--     foreign key (post_id) references posts(id)
+-- );
